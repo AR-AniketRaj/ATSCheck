1532279486e33f442a0e80396f52a0042f531870
Aniket <aniketraj8832@gmail.com>
2026-06-05 22:15:29 +0530

    auth setup

---
+++ backend/models/userModel.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+});
+
+userSchema.pre("save", async function () {
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+// module.exports = { userVerification };
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,34 @@
+require("dotenv").config();
 const express = require("express");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/AuthRoute");
+const profileRoutes = require("./routes/profile");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: ["http://localhost:5000"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+)
+
+connectDB();
 
 app.get("/", (req, res) => {
-    res.send("ATS Backend Running");
+  res.send("ATS Backend Running");
 });
 
-app.listen(5000, () => {
-    console.log("Server Running");
+app.use("/", authRoutes);
+app.use("/profile", profileRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server Running on ${PORT}`);
 });
