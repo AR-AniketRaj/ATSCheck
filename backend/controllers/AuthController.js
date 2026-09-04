9cabebe6a435d527a4db52f44fe5c1958bd02269
Aniket <aniketraj8832@gmail.com>
2026-09-04 14:39:05 +0530

    fix backend model path

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -1,4 +1,4 @@
-const User = require("../models/UserModel");
+const User = require("../models/userModel");
 const { createSecretToken } = require("../util/SecretToken");
 const bcrypt = require("bcryptjs");
 const crypto = require("crypto");
